ded7e2647f038bc91f66c8ef6d0fa73d5fa4d80f
chimo <chimo@chromic.org>
2023-10-30 00:12:20 -0400

    list-container-updates: Handle "alpinelinux" with "Alpine"

--- list-container-updates.sh
+++ list-container-updates.sh
@@ -43,7 +43,7 @@ main() (
         os="${container#*,}"
 
         case "${os}" in
-            "Alpine")
+            "Alpine"|"alpinelinux")
                 results=$(apk "${name}")
                 ;;
             "Debian")
