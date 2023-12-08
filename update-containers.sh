9ddca577cca230c38d50085cad2d2ba5816fee3f
chimo <chimo@chromic.org>
2023-12-08 16:51:17 -0500

    update-container: Add getopts, some refactoring

--- update-containers.sh
+++ update-containers.sh
@@ -1,20 +1,10 @@
-#!/bin/sh -e
+#!/bin/sh -eu
 
 apk() (
     container="${1}"
 
-    lxc exec "${container}" -- sh -c "apk update -q" < /dev/null
-
-    updates=$(
-        lxc exec "${container}" -- sh -c "apk list -u | wc -l" < /dev/null
-    )
-
-    if [ "${updates}" -gt 0 ]; then
-        cmd="apk upgrade"
-        upgrade "${container}" "${cmd}"
-    else
-        echo "Nothing to do..."
-    fi
+    cmd="apk upgrade"
+    upgrade "${container}" "${cmd}"
 )
 
 
@@ -30,48 +20,53 @@ snapshot() (
     container="${1}"
     snapshot_name="pre-patch-$(date +%Y%m%d)"
 
-    echo "Snapshotting ${container}..."
-
-    if ! lxc snapshot "${name}" "${snapshot_name}"; then
+    if ! lxc snapshot "${container}" "${snapshot_name}"; then
         echo "${container} failed to snapshot. Skipping upgrade..."
         return 1
     fi
 )
 
 
-_upgrade() (
-    container="${1}"
+upgrade() (
+    name="${1}"
     cmd="${2}"
 
-    echo "Upgrading ${container}..."
-
-    if ! lxc exec "${container}" -- sh -c "${cmd}" < /dev/null; then
-        echo "Upgrade failed"
-        return
+    if ! lxc exec "${name}" -- sh -c "${cmd}" < /dev/null; then
+        return 1
     fi
 )
 
 
-upgrade() (
-    container="${1}"
-    cmd="${2}"
+check_for_updates() (
+    name="${1}"
+    os="${2}"
 
-    if ! snapshot "${container}"; then
-        echo "Snapshot failed, skipping upgrade..."
-    else
-        _upgrade "${container}" "${cmd}"
+    case "${os}" in
+        "Alpine"|"alpinelinux")
+            lxc exec "${name}" -- sh -c "apk update -q" < /dev/null > /dev/null
 
-        echo "Restarting ${container}..."
-        lxc restart "${container}"
-    fi
+            updates=$(
+                lxc exec "${name}" -- sh -c "apk list -u | wc -l" < /dev/null
+            )
+            ;;
+        "Debian")
+            lxc exec "${name}" -- sh -c "apk update" < /dev/null > /dev/null
+
+            updates=$(
+                lxc exec "${name}" -- sh -c "apk list --upgradable" < /dev/null
+            )
+            ;;
+        *)
+            ;;
+    esac
+
+    echo "${updates}"
 )
 
 
 update() (
     name="${1}"
     os="${2}"
-
-    echo "Processing ${name}..."
 
     case "${os}" in
         "Alpine"|"alpinelinux")
@@ -95,7 +90,7 @@ update_all() (
         name="${container%,*}"
         os="${container#*,}"
 
-        update "${name}" "${os}"
+        handle "${restart}" "${snapshot}" "${name}" "${os}"
         echo ""
     done <<EOF
 $containers
@@ -104,21 +99,108 @@ EOF
 
 
 update_specific() (
-    containers="${1}"
+    restart="${1}"
+    snapshot="${2}"
+    containers="${3}"
 
     while IFS= read -r container
     do
         name="${container}"
         os=$(lxc list name="${name}" -c config:image.os --format csv)
 
-        update "${name}" "${os}"
+        handle "${restart}" "${snapshot}" "${name}" "${os}"
     done <<EOF
 $containers
 EOF
 )
 
 
+usage() (
+    echo "Usage: ${0} [-n]" 1>&2
+    exit 1
+)
+
+
+argparse() (
+    post_upgrade=1
+    restart=1
+    snapshot=1
+
+    while getopts ':PRS' opt
+    do
+        case $opt in
+            P)
+                post_upgrade=0
+                ;;
+            R)
+                restart=0
+                ;;
+            S)
+                snapshot=0
+                ;;
+            *)
+                usage
+                ;;
+        esac
+    done
+
+    shift "$((OPTIND - 1))"
+
+    main "${post_upgrade}" "${restart}" "${snapshot}" "${*}"
+)
+
+
+handle() (
+    restart="${1}"
+    take_snapshot="${2}"
+    name="${3}"
+    os="${4}"
+
+    echo "Processing ${name}..."
+
+    # Check for updates
+    nb_updates=$(check_for_updates "${name}" "${os}")
+
+    # Bail if no updates are pending
+    if [ "${nb_updates}" -eq 0 ]; then
+        echo "Nothing to do."
+        return 0
+    fi
+
+    # Snapshot
+    if [ "${take_snapshot}" -ne 0 ]; then
+        echo "Snapshotting ${name}..."
+
+        if ! snapshot "${name}"; then
+            echo "Snapshot failed, skipping update"
+
+            return 1
+        fi
+    fi
+
+    # Update
+    echo "Upgrading ${name}..."
+    if ! update "${name}" "${os}"; then
+        echo "Update failed"
+
+        return 1
+    fi
+
+    # Restart
+    echo "Restarting ${name}..."
+    if [ "${restart}" -ne 0 ]; then
+        lxc restart "${name}"
+    fi
+)
+
+
 main() (
+    # Params
+    post_upgrade="${1}"
+    restart="${2}"
+    take_snapshot="${3}"
+    containers="${4}"
+
     # Paths
     main_dir=$(dirname -- "$( readlink -f -- "$0"; )")
     config_file="${main_dir}/config"
@@ -128,22 +210,19 @@ main() (
         . "${config_file}"
     fi
 
-    # Upgrade
-    if [ "${1}" = "--containers" ]; then
-        shift
-        containers=$(echo "${@}" | tr ' ' '\n')
-
-        update_specific "${containers}"
+    if [ -n "${containers}" ]; then
+        containers=$(echo "${containers}" | tr ' ' '\n')
+        update_specific "${restart}" "${take_snapshot}" "${containers}"
     else
-        update_all
+        update_all "${restart}" "${take_snapshot}"
     fi
 
     # Run post-upgrade command
-    if [ -n "${POST_UPGRADE_SCRIPT}" ]; then
+    if [ -n "${POST_UPGRADE_SCRIPT}" ] && [ "${post_upgrade}" -ne 0 ]; then
         sh -c "${POST_UPGRADE_SCRIPT}"
     fi
 )
 
 
-main "${@}"
+argparse "${@}"
 
