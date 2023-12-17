5e0f32229b86d596856de5a2d762183429f0c302
chimo <chimo@chromic.org>
2023-12-17 11:08:06 -0500

    create-containers.sh: Support lxc "project"s and custom config dirs
    This needs cleanup, but seems to work.

--- create-container/create-container.sh
+++ create-container/create-container.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -eux
 
 profile_exists() (
     profile="${1}"
@@ -82,6 +82,7 @@ create_container() (
     name="${1}"
     image="${2}"
     types=$(echo "${3}" | tr ' ' '\n')
+    lxc_project="${4}"
 
     # Define some paths
     script_dir=$(dirname -- "$( readlink -f -- "$0"; )")
@@ -97,11 +98,24 @@ create_container() (
         ensure_profiles_exist "${name}" "${containers_dir}"
     fi
 
+    lxc project switch "${lxc_project}"
+
     # Create container
     lxc launch "${image}" "${name}"
 
     # wait for network
     sleep 5s
+)
+
+
+configure_container() (
+    # Params
+    name="${1}"
+    config_dir="${2}"
+    types="${3}"
+
+    # Paths
+    script_dir=$(dirname -- "$( readlink -f -- "$0"; )")
 
     # Common post-launch commands
     lxc exec "${name}" -- sh < "${script_dir}/common/post-launch.sh"
@@ -112,8 +126,8 @@ create_container() (
     fi
 
     # Container post-launch commands
-    if [ -e "${containers_dir}/${name}/assets/post-launch.sh" ]; then
-        "${containers_dir}/${name}/assets/post-launch.sh" "${name}"
+    if [ -e "${config_dir}/assets/post-launch.sh" ]; then
+        "${config_dir}/assets/post-launch.sh" "${name}"
     fi
 
     # "Types" post-config commands
@@ -122,32 +136,87 @@ create_container() (
     fi
 
     # Container post-config commands
-    if [ -e "${containers_dir}/${name}/assets/remote.sh" ]; then
-        lxc exec "${name}" -- sh < "${containers_dir}/${name}/assets/remote.sh"
+    if [ -e "${config_dir}/assets/remote.sh" ]; then
+        lxc exec "${name}" -- sh < "${config_dir}/assets/remote.sh"
     fi
 )
 
 
 usage() (
-    echo "Usage ${0} <name>" 1>&2
+    message=""
+    newline="
+"
+
+    while IFS= read -r line
+    do
+        message="${message}${line}${newline}"
+    done <<EOF
+Usage: ${0} [-h] [-p] NAME
+            [-h] [-p] -f [NAME]
+
+options:
+-h      show this help message and exit
+-d      container settings directory
+-p      lxc project
+EOF
+
+    echo "${message}" 1>&2
     exit 1
 )
 
 
-main() (
-    if [ "${#}" -lt 1 ]; then
+argparse() (
+    config_dir=""
+    lxc_project="default"
+
+    while getopts ':d:p:' opt
+    do
+        case $opt in
+            d)
+                config_dir="${OPTARG-}"
+                ;;
+            p)
+                lxc_project="${OPTARG-default}"
+                ;;
+            *)
+                usage
+                ;;
+        esac
+    done
+
+    shift "$((OPTIND - 1))"
+
+    name="${1-}"
+
+    if [ -z "${config_dir}" ] && [ -z "${name}" ]; then
         usage
     fi
 
+    main "${name}" "${config_dir}" "${lxc_project}"
+)
+
+
+main() (
     # Params
     name="${1}"
+    config_dir="${2}"
+    lxc_project="${3}"
+
+    # Options
     image=""
     profiles=""
 
     # Define some paths
     script_dir=$(dirname -- "$( readlink -f -- "$0"; )")
     containers_dir="${script_dir}/containers"
-    config_file="${containers_dir}/${name}/config"
+
+    if [ -z "${config_dir}" ]; then
+        config_dir="${containers_dir}/${name}"
+    else
+        config_dir=$(readlink -f -- "${config_dir}")
+    fi
+
+    config_file="${config_dir}/config"
 
     if [ ! -f "${config_file}" ]; then
         echo "Config file ${config_file} doesn't exist"
@@ -162,30 +231,49 @@ main() (
         fi
 
         # This is pretty unforgiving syntax-wise; we're not `trim`ing anything
-        # or doing any other sort of sanitization...
+        # or doing any other sort of sanitation...
         key="${line%=*}"
         value="${line#*=}"
 
         case $key in
+            name)
+                cname="${value}"
+                ;;
             image)
                 image="${value}"
-            ;;
+                ;;
             profiles)
                 profiles="${value}"
-            ;;
+                ;;
+            project)
+                cproject="${value}"
+                ;;
             *)
                 echo "Invalid config key '${key}'"
-            ;;
+                ;;
         esac
     done 3< "${config_file}"
+
+    # If "name" wasn't passed as a parameter, use the value from the config
+    # file
+    if [ -z "${name}" ]; then
+        name="${cname}"
+    fi
+
+    # If "project" wasn't passed as a parameter, use the value from the config
+    # file
+    if [ -z "${lxc_project}" ]; then
+        lxc_project="${cproject}"
+    fi
 
     if [ -z "${image}" ]; then
         echo "Config file must contain an 'image' property"
         exit 1
     fi
 
-    create_container "${name}" "${image}" "${profiles}"
+    create_container "${name}" "${image}" "${profiles}" "${lxc_project}"
+    configure_container "${name}" "${config_dir}" "${profiles}"
 )
 
-main "${@}"
+argparse "${@}"
 
