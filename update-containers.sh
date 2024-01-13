b1220641eedf475209446e66fee49880e313b647
chimo <chimo@chromic.org>
2024-01-13 14:57:05 -0500

    [WIP] Support dist-ugrade
    Within update-containers.sh instead of a separate script.

--- update-containers.sh
+++ update-containers.sh
@@ -1,18 +1,105 @@
 #!/bin/sh -eu
 
+LATEST_ALPINE_VERSION=""
+
+get_latest_alpine_version() (
+    if [ -z "${LATEST_ALPINE_VERSION}" ]; then
+        LATEST_ALPINE_VERSION=$(
+            git -c 'versionsort.suffix=-' \
+                ls-remote --exit-code --refs --sort='version:refname' \
+                --tags https://git.alpinelinux.org/aports '*.*.*' \
+                | grep -vE '[0-9]+\.[0-9]+_rc' \
+                | tail -n 1 \
+                | cut -d '/' -f3 \
+                | grep -oE '[0-9]+\.[0-9]+'
+        )
+    fi
+
+    echo "${LATEST_ALPINE_VERSION}"
+)
+
+
+get_container_version() (
+    container="${1}"
+    cmd="grep 'VERSION_ID=' /etc/os-release | grep -oE '[0-9]+\.[0-9]+'"
+
+    container_os_version=$(
+        lxc exec "${container}" -- sh -c "${cmd}" < /dev/null
+    )
+
+    echo "${container_os_version}"
+)
+
+
+check_dist_upgrade() (
+    container="${1}"
+    latest_alpine_version=$(get_latest_alpine_version)
+    container_os_version=$(get_container_version "${container}")
+
+    needs_dist_upgrade=0
+
+    if [ 1 -eq "$(echo "${latest_alpine_version} > ${container_os_version}" | bc)" ]; then
+        needs_dist_upgrade=1
+    fi
+
+    echo "${needs_dist_upgrade}"
+)
+
+
 apk() (
     container="${1}"
+    dist_upgrade="${2}"
 
-    cmd="apk upgrade"
-    upgrade "${container}" "${cmd}"
+    cmd="apk update && apk upgrade"
+
+    echo "Upgrading ${name}..."
+    lxc_exec "${container}" "${cmd}"
+
+    if [ "${dist_upgrade}" -eq 1 ]; then
+        needs_dist_upgrade=$(check_dist_upgrade "${container}")
+
+        if [ "${needs_dist_upgrade}" -eq 0 ]; then
+            echo "dist-upgrade not needed; running latest alpine."
+            return 0
+        fi
+
+        latest_alpine_version=$(get_latest_alpine_version)
+
+        # Change the repo URLs
+        echo "Updating repo URLs..."
+        change_repo_urls "${container}" "${latest_alpine_version}"
+
+        # dist-upgrade
+        cmd="apk update && apk upgrade -a"
+        echo "Performing dist-upgrade..."
+        lxc_exec "${container}" "${cmd}"
+    fi
+)
+
+
+change_repo_urls() (
+    name="${1}"
+    latest_version="${2}"
+
+    repo_file="/etc/apk/repositories"
+
+    lxc exec "${name}" -- sh -c \
+        "sed -i -E \"s#/alpine/v[0-9].[0-9]+/#/alpine/v${latest_version}/#g\" \
+        \"${repo_file}\"" < /dev/null
 )
 
 
 apt() (
     container="${1}"
+    dist_upgrade="${2}"
+
     cmd="apt update && apt upgrade -y"
 
-    upgrade "${container}" "${cmd}"
+    lxc_exec "${container}" "${cmd}"
+
+    if [ "${dist_upgrade}" -eq 1 ]; then
+        echo "dist-upgrade not implemented"
+    fi
 )
 
 
@@ -27,7 +114,7 @@ snapshot() (
 )
 
 
-upgrade() (
+lxc_exec() (
     name="${1}"
     cmd="${2}"
 
@@ -67,13 +154,14 @@ check_for_updates() (
 update() (
     name="${1}"
     os="${2}"
+    dist_upgrade="${3}"
 
     case "${os}" in
         "Alpine"|"alpinelinux")
-            apk "${name}"
+            apk "${name}" "${dist_upgrade}"
             ;;
         "Debian")
-            apt "${name}"
+            apt "${name}" "${dist_upgrade}"
             ;;
         *)
             echo "Unknown OS: ${os}"
@@ -83,6 +171,10 @@ update() (
 
 
 update_running_containers() (
+    restart=${1}
+    snapshot="${2}"
+    dist_upgrade="${3}"
+
     containers=$(lxc list status=running -c n,config:image.os --format csv)
 
     while IFS= read -r container
@@ -90,7 +182,7 @@ update_running_containers() (
         name="${container%,*}"
         os="${container#*,}"
 
-        handle "${restart}" "${snapshot}" "${name}" "${os}"
+        handle "${restart}" "${snapshot}" "${name}" "${os}" "${dist_upgrade}"
         echo ""
     done <<EOF
 $containers
@@ -99,8 +191,11 @@ EOF
 
 
 update_stopped_containers() (
-    containers=$(lxc list status=stopped -c n,config:image.os --format csv)
     restart=0
+    snapshot="${1}"
+    dist_upgrade="${2}"
+
+    containers=$(lxc list status=stopped -c n,config:image.os --format csv)
 
     while IFS= read -r container
     do
@@ -110,7 +205,7 @@ update_stopped_containers() (
         echo "Starting ${name}..."
         lxc start "${name}"
 
-        handle "${restart}" "${snapshot}" "${name}" "${os}"
+        handle "${restart}" "${snapshot}" "${name}" "${os}" "${dist_upgrade}"
 
         echo "Stopping ${name}..."
         lxc stop "${name}"
@@ -125,14 +220,15 @@ EOF
 update_specific() (
     restart="${1}"
     snapshot="${2}"
-    containers="${3}"
+    dist_upgrade="${3}"
+    containers="${4}"
 
     while IFS= read -r container
     do
         name="${container}"
         os=$(lxc list name="${name}" -c config:image.os --format csv)
 
-        handle "${restart}" "${snapshot}" "${name}" "${os}"
+        handle "${restart}" "${snapshot}" "${name}" "${os}" "${dist_upgrade}"
     done <<EOF
 $containers
 EOF
@@ -148,10 +244,11 @@ usage() (
     do
         message="${message}${line}${newline}"
     done <<EOF
-Usage: ${0} [-a] [-h] [-P] [-R] [-S]
+Usage: ${0} [-a] [-d] [-h] [-P] [-R] [-S]
 
 options:
 -a      all containers (stopped containers will be started, upgraded and stopped)
+-d      perform a "dist-upgrade"
 -h      show this help message and exit
 -P      do not run the post-upgrade script
 -R      do not restart the container after upgrade
@@ -165,15 +262,19 @@ EOF
 
 argparse() (
     all_containers=0
+    dist_upgrade=0
     post_upgrade=1
     restart=1
     snapshot=1
 
-    while getopts ':aPRS' opt
+    while getopts ':adPRS' opt
     do
         case $opt in
             a)
                 all_containers=1
+                ;;
+            d)
+                dist_upgrade=1
                 ;;
             P)
                 post_upgrade=0
@@ -192,7 +293,8 @@ argparse() (
 
     shift "$((OPTIND - 1))"
 
-    main "${post_upgrade}" "${restart}" "${snapshot}" "${all_containers}" "${*}"
+    main "${post_upgrade}" "${restart}" "${snapshot}" "${all_containers}" \
+        "${dist_upgrade}" "${*}"
 )
 
 
@@ -201,6 +303,7 @@ handle() (
     take_snapshot="${2}"
     name="${3}"
     os="${4}"
+    dist_upgrade="${4}"
 
     echo "Processing ${name}..."
 
@@ -226,7 +329,7 @@ handle() (
 
     # Update
     echo "Upgrading ${name}..."
-    if ! update "${name}" "${os}"; then
+    if ! update "${name}" "${os}" "${dist_upgrade}"; then
         echo "Update failed"
 
         return 1
@@ -246,7 +349,8 @@ main() (
     restart="${2}"
     take_snapshot="${3}"
     all_containers="${4}"
-    containers="${5}"
+    dist_upgrade="${5}"
+    containers="${6}"
 
     # Paths
     main_dir=$(dirname -- "$( readlink -f -- "$0"; )")
@@ -259,12 +363,12 @@ main() (
 
     if [ -n "${containers}" ]; then
         containers=$(echo "${containers}" | tr ' ' '\n')
-        update_specific "${restart}" "${take_snapshot}" "${containers}"
+        update_specific "${restart}" "${take_snapshot}" "${dist_upgrade}" "${containers}"
     else
-        update_running_containers "${restart}" "${take_snapshot}"
+        update_running_containers "${restart}" "${take_snapshot}" "${dist_upgrade}"
 
         if [ "${all_containers}" -eq 1 ]; then
-            update_stopped_containers "${take_snapshot}"
+            update_stopped_containers "${take_snapshot}" "${dist_upgrade}"
         fi
     fi
 
