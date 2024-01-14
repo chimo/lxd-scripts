d20d277351c7b3995fbae6f309fd9f7785cf419a
chimo <chimo@chromic.org>
2024-01-13 22:56:13 -0500

    qutebrowser: Install "mesa-dri-gallium"
    Seems like we need this now

--- create-container/containers/qutebrowser/assets/remote.sh
+++ create-container/containers/qutebrowser/assets/remote.sh
@@ -2,5 +2,6 @@
 
 # Install qutebrowser
 # We also install vim and a terminal so things like ":edit-url" work
-apk add qutebrowser foot vim
+# We also need mesa-dri-gallium so the GUI works
+apk add qutebrowser foot vim mesa-dri-gallium
 
